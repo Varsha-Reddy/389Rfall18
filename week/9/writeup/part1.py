9ea16d0b70f5619ed56b770c7c70fdc922ebc283
Varsha-Reddy <42913716+Varsha-Reddy@users.noreply.github.com>
2018-11-13 20:41:54 -0500

    Update part1.py

--- week/9/writeup/part1.py
+++ week/9/writeup/part1.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
 
+# importing a useful library -- feel free to add any others you find necessary
+import hashlib
+import string
+
+# this will work if you place this script in your writeup folder
+hashlist = open('../hashes','r').read().split()
+wordlist = open("../probable-v2-top1575.txt", 'r')
+# a string equal to 'abcdefghijklmnopqrstuvwxyz'.
+salts = string.ascii_lowercase
+
+for hash in hashlist:
+    hash = hash.strip()
+    for word in wordlist:
+        for salt in salts:
+            password = hashlib.sha512(salt + word.rstrip()).hexdigest()
+            if password in hashlist:
+                print "Password: " + word.strip() 
+                print "Salt: " + salt
